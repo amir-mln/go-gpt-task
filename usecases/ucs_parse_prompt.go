21359984338e45282a91ab4dbb484c734a60ea4e
amir-mln <molaeyan.career@gmail.com>
2024-10-09 10:00:31 +0330

    refactoring

--- usecases/ucs_parse_prompt.go
+++ usecases/ucs_parse_prompt.go
@@ -27,50 +27,53 @@ func (uc *usecases) ParsePrompt(ctx context.Context, prompt string) (models.Lapt
 		return models.Laptop{}, errors.New("invalid empty string as prompt")
 	}
 
+	// mx := sync.RWMutex{}
+	// mx.
 	if cached, ok := uc.cacheRepo.FindByKey(prompt); ok {
 		if cached.Failed {
 			return models.Laptop{}, errors.New(cached.Value)
 		}
 
-		if laptop, ok := uc.dbRepo.FindByID(cached.Value); !ok {
+		laptop, ok := uc.dbRepo.FindByID(cached.Value)
+		if !ok {
 			return models.Laptop{}, fmt.Errorf(
 				"found a non existing cached laptop with id %q",
 				cached.Value,
 			)
-		} else {
-			return laptop, nil
 		}
-
-	}
-
-	if resp, err := uc.aiParser.Parse(ctx, prompt); err == nil {
-		if resp.Failed {
-			uc.cacheRepo.Insert(
-				prompt,
-				CachedLaptopPrompt{Failed: true, Value: resp.Message, Prompt: prompt},
-			)
-			return models.Laptop{}, errors.New(resp.Message)
-		}
-
-		laptop := resp.Laptop
-		laptop.ID = uuid.NewString()
-		if err := laptop.Validate(); err != nil {
-			uc.cacheRepo.Insert(
-				prompt,
-				CachedLaptopPrompt{Failed: true, Prompt: prompt, Value: err.Error()},
-			)
-
-			return models.Laptop{}, err
-		}
-
-		uc.cacheRepo.Insert(
-			prompt,
-			CachedLaptopPrompt{Failed: false, Value: laptop.ID, Prompt: prompt},
-		)
-		uc.dbRepo.Insert(laptop)
 
 		return laptop, nil
-	} else {
+	}
+
+	resp, err := uc.aiParser.Parse(ctx, prompt)
+	if err != nil {
 		return models.Laptop{}, err
 	}
+
+	if resp.Failed {
+		uc.cacheRepo.Insert(
+			prompt,
+			CachedLaptopPrompt{Failed: true, Value: resp.Message, Prompt: prompt},
+		)
+		return models.Laptop{}, errors.New(resp.Message)
+	}
+
+	laptop := resp.Laptop
+	laptop.ID = uuid.NewString()
+	if err := laptop.Validate(); err != nil {
+		uc.cacheRepo.Insert(
+			prompt,
+			CachedLaptopPrompt{Failed: true, Prompt: prompt, Value: err.Error()},
+		)
+
+		return models.Laptop{}, err
+	}
+
+	uc.cacheRepo.Insert(
+		prompt,
+		CachedLaptopPrompt{Failed: false, Value: laptop.ID, Prompt: prompt},
+	)
+	uc.dbRepo.Insert(laptop)
+
+	return laptop, nil
 }
